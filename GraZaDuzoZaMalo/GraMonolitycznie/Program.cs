b9b5a708c4758b7043484f24c5a14ff2f9705207
kamil170597 <48652200+kamil170597@users.noreply.github.com>
2019-03-17 17:13:49 +0100

    Version 1

---
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraMonolitycznie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj!");
+            Console.Write("POdaj swoje imie:");
+            string imie=Console.ReadLine();
+            Console.WriteLine($"Czesc {imie}");
+            
+
+        }
+    }
+}
