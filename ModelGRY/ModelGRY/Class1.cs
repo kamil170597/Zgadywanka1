ed5ebb240120e4efb680e40fd7f3bd8c9284ae21
kamil170597 <48652200+kamil170597@users.noreply.github.com>
2019-05-19 17:05:18 +0200

    cos

--- ModelGRY/ModelGRY/Class1.cs
+++ ModelGRY/ModelGRY/Class1.cs
@@ -2,7 +2,32 @@
 
 namespace ModelGRY
 {
-    public class Class1
+    public class Gra
     {
+        //pola
+        int zakresOd, zakresDo;
+        int wylosowana;
+        //historia gry
+
+        //konstruktory
+        public Gra(int a, int b)
+        {
+            zakresOd = Math.Min(a,b);
+            zakresDo = Math.Max(b,a);
+            wylosowana = Losuj(zakresOd, zakresDo);
+        }
+        //losowanie
+        public static int Losuj(int min = 1, int max = 100)
+        {
+            Random generator = new Random();
+            if (min > max)
+            {
+                int tmp = min;
+                min = max;
+                max = tmp;
+            }
+
+            return generator.Next(min, max + 1);
+        }
     }
 }
---
+++ UnitTestsProjectModelGry/UnitTestsProjectModelGry/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestsProjectModelGry")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestsProjectModelGry")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bca4b9d4-4b2c-4a98-b967-f7511113b41f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
